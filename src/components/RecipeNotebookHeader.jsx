a2f28e2c3486d6d55cc3a9e6a8ad41b0a63f1d03
shachar adi <shacharadi32@gmail.com>
2026-05-15 16:12:51 +0300

    Remove public image assets

--- src/components/RecipeNotebookHeader.jsx
+++ src/components/RecipeNotebookHeader.jsx
@@ -7,13 +7,6 @@ export default function RecipeNotebookHeader() {
         <span>🥄</span>
       </div>
 
-      <img
-        className="hero-illustration"
-        src="/recipe-notebook-illustration.png"
-        alt=""
-        aria-hidden="true"
-      />
-
       <h1>אמא, הבטחת לנו עוף לשבת 🍗</h1>
       <p className="description">
         מחברת מתכונים חכמה שמונעת משברי רעב, שאלות מיותרות, וקריאות מצוקה לחסדי נעמי.
